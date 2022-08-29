4e4c9e715f8ce71a983a5e47ac0b9cfcd058c7d9
MATarg <matiastula@gmail.com>
2022-08-29 13:53:32 -0300

    Actualizanding

---
+++ src/tasks.js
@@ -0,0 +1,19 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Tarea",
+    description: "Tarea a realizar",
+  },
+
+  {
+    id: 1,
+    title: "Tarea",
+    description: "Tarea a realizar",
+  },
+
+  {
+    id: 2,
+    title: "Tarea",
+    description: "Tarea a realizar",
+  },
+];
